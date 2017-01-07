4ffd3237cec49040dfabde93317dd392f25cc8c5
Maximiliaan Strother III <maxbax@op.pl>
2017-01-07 19:01:55 +0100

    GROW MY MINION, GROWgit add hello/main.go !

--- hello/main.go
+++ hello/main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"time"
 )
 
 func main() {
-	var args []string 							//manual type declaration   1/2
-    args = os.Args								// -||- 				    2/2
-	hourOfDay := time.Now().Hour()				//type inference
-	greeting := getGreeting(hourOfDay)
-
+	var args []string              		//manual type declaration   1/2
+	args = os.Args                		// -||- 				    2/2
+	hourOfDay := time.Now().Hour() 		//type inference
+	greeting, err := getGreeting(hourOfDay)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	if len(args) > 1 {
-		fmt.Println(args[1])
+		if len(args[1]) < 3 {
+			for i := 0; i < 3; i++ {
+				fmt.Println(args[1])
+			}
+		} else {
+			i := 0
+			isLessThanSix := true
+			for isLessThanSix {
+				if i >= 6 {
+					isLessThanSix = false
+				} else if i == 1 {
+					fmt.Println("Fear my spamming technique!!!")
+				}
+				fmt.Println(args[1])
+				i++
+			}
+		}
 	} else {
 		fmt.Println(greeting)
 	}
 }
 
-func getGreeting(hour int) string {
-	if hour < 12 {
-		return "Good Morning Sweetheart"
-	} else if hour < 18 {
-		return "Splendid Afternoon You Beautiful Being"
+func getGreeting(hour int) (string, error) {
+	var message string
+	if hour < 4 {
+		err := errors.New("Khhhhh!! Go back to sleep! Programs also need some rest")
+		return message, err
+	} else if hour < 7 {
+		message = "Good Morning Sweetheart"
+	} else if hour < 16 {
+		message = "Splendid Afternoon You Beautiful Being"
 	} else {
-		return "Dark Evening Your Vampiresy"
+		message = "Dark Evening Your Vampiresy"
 	}
+	return message, nil
 }
